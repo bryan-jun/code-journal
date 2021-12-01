da7593d3777429b699b4608a4ed911dc6afac49c
Bryan Jun <bryjun@gmail.com>
2021-11-30 22:19:01 -0800

    completed feature 1 - adding entries

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 /* global data */
 /* exported data */
+var photoUrl = document.getElementById('photo');
+var photo = document.querySelector('.image');
+var journalForm = document.getElementById('journal');
+
+photoUrl.addEventListener('input', function (event) {
+  photo.setAttribute('src', journalForm.elements.photo.value);
+
+}, false);
+
+journalForm.addEventListener('submit', function (event) {
+  var entry = {
+    title: journalForm.elements.title.value,
+    photoP: journalForm.elements.photo.value,
+    notesP: journalForm.elements.notes.value
+
+  };
+
+  entry.nextEntryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  data.entries.unshift(entry);
+  photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  journalForm.getElementById('title').reset();
+  journalForm.getElementById('photo').reset();
+  journalForm.getElementById('notes').reset();
+
+}, false);
